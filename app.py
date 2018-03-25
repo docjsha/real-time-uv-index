65a53cc39976248b2475bc21f4fc351bed575f42
docjsha <jing.sha@gmail.com>
2018-03-25 15:34:14 -0500

    updated

--- app.py
+++ app.py
@@ -1,22 +1,7 @@
 # -*- coding:utf8 -*-
 # !/usr/bin/env python
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 from __future__ import print_function
-#from future.standard_library import install_aliases
-#install_aliases()
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
@@ -106,8 +91,4 @@ def makeQuery(req):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
-
-    print("Starting app on port %d" % port)
-
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(host='127.0.0.1', port=8080, debug=True)
